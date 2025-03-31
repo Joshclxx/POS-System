5115c7d2ad2aa29294c8e101fe7cb59c0e179b7c
Joshclxx <joshuacolobong11@gmail.com>
2025-03-31 15:50:40 +0800

    Update OrderesMenu Component

--- src/components/orders/OrdersView.tsx
+++ src/components/orders/OrdersView.tsx
@@ -1,25 +1,44 @@
+// OrdersView.tsx
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import SectionContainer from "../SectionContainer";
 import Button from "../Button";
 import Image from "next/image";
 import { useOrderStore } from "@/hooks/useOrder";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { Dispatch, SetStateAction } from "react";
 
-const OrdersView = () => {
+interface OrdersViewProps {
+  isPaying: boolean;
+  setIsPaying: Dispatch<SetStateAction<boolean>>;
+  setTotal: Dispatch<SetStateAction<number>>;
+  onClearOrders: () => void;
+}
+
+const OrdersView: React.FC<OrdersViewProps> = ({
+  isPaying,
+  setIsPaying,
+  setTotal,
+  onClearOrders,
+}) => {
   const [selectedOption, setSelectedOption] = useState("DINE IN");
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedIndexes, setSelectedIndexes] = useState<number[]>([]);
+  const [isEditingQty, setIsEditingQty] = useState(false);
+  const [tempQty, setTempQty] = useState<number | null>(null);
 
   const options = ["DINE IN", "TAKE OUT"];
-
   const selectedProducts = useOrderStore((state) => state.selectedProducts);
   const total = selectedProducts.reduce(
     (acc, item) => acc + item.price[item.size] * (item.quantity || 1),
     0
   );
 
-  // Properly implemented size name converter
+  useEffect(() => {
+    setTotal(total);
+  }, [total, setTotal]);
+
   const getSizeName = (size: string) => {
     switch (size) {
       case "PT":
@@ -33,89 +52,94 @@ const OrdersView = () => {
     }
   };
 
-  const [selectedIdx, setSelectedIdx] = useState<number | null>(null);
+  const longPressTimeout = useRef<NodeJS.Timeout | null>(null);
 
-  const removeProduct = useOrderStore((state) => state.removeProduct);
+  const handleLongPress = (idx: number) => {
+    setSelectedIndexes([idx]);
+    setIsEditingQty(true);
+    setTempQty(selectedProducts[idx]?.quantity || 1);
+  };
 
-  const [isEditingQty, setIsEditingQty] = useState(false);
-  const [tempQty, setTempQty] = useState<number | null>(null);
+  const startLongPress = (idx: number) => {
+    longPressTimeout.current = setTimeout(() => handleLongPress(idx), 600);
+  };
+
+  const cancelLongPress = () => {
+    if (longPressTimeout.current) {
+      clearTimeout(longPressTimeout.current);
+      longPressTimeout.current = null;
+    }
+  };
 
   return (
     <SectionContainer background="mt-1 w-full max-w-[428px] h-[914px]">
-      {/* DELETE & DINE IN */}
-      <div className="flex gap-[4px] w-full">
-        <Button
-          variant="view-dlt"
-          isSave={selectedIdx !== null}
-          className="flex-1"
-          onClick={() => {
-            if (selectedIdx !== null) {
-              removeProduct(selectedIdx);
-              setSelectedIdx(null);
-              setIsEditingQty(false);
-              setTempQty(null);
-            }
-          }}
+      <AnimatePresence mode="wait">
+        <motion.div
+          key="dlt"
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.3 }}
         >
-          DLT
-        </Button>
-        <Button
-          variant="view-qty"
-          isSave={selectedIdx !== null || isEditingQty}
-          className="flex-1"
-          onClick={() => {
-            if (isEditingQty && selectedIdx !== null && tempQty !== null) {
-              useOrderStore.setState((state) => {
-                const update = [...state.selectedProducts];
-                update[selectedIdx].quantity = tempQty;
-                return { selectedProducts: update };
-              });
-              setSelectedIdx(null);
-              setIsEditingQty(false);
-              setTempQty(null);
-            } else if (selectedIdx !== null) {
-              setIsEditingQty(true);
-              setTempQty(selectedProducts[selectedIdx].quantity || 1);
-            }
-          }}
-        >
-          {isEditingQty ? "Save" : "QTY"}
-        </Button>
-        <div className="relative w-[212px]">
-          <button
-            className="flex items-center justify-between p-2 bg-colorBlue w-full h-[60px] text-white font-semibold"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            <span className="text-lg">{selectedOption}</span>
-            <Image
-              src="/icon/dropdown.svg"
-              alt="DropDown"
-              height={15}
-              width={15}
-            />
-          </button>
-          {isOpen && (
-            <div className="absolute top-full left-0 w-full bg-primary border border-primaryGray shadow-lg z-10">
-              {options.map((option) => (
-                <button
-                  key={option}
-                  className={`w-full text-left p-2 text-black hover:bg-gray-200 ${
-                    selectedOption === option ? "font-bold" : ""
-                  }`}
-                  onClick={() => {
-                    setSelectedOption(option);
-                    setIsOpen(false);
-                  }}
-                >
-                  {option}
-                </button>
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
+          <div className="flex gap-[4px] w-full max-w-[428px]">
+            <div className="flex justify-between gap-[4px] w-full">
+              <Button
+                variant="view-dlt"
+                isSave={selectedIndexes.length > 0}
+                className="flex-1 w-full"
+                onClick={() => {
+                  if (selectedIndexes.length > 0) {
+                    useOrderStore.setState((state) => {
+                      const updated = state.selectedProducts.filter(
+                        (_, idx) => !selectedIndexes.includes(idx)
+                      );
+                      return { selectedProducts: updated };
+                    });
+                    setSelectedIndexes([]);
+                    setIsEditingQty(false);
+                    setTempQty(null);
+                  }
+                }}
+              >
+                DELETE
+              </Button>
 
-      {/* ORDER LIST */}
+              <div className="relative flex-1">
+                <button
+                  className="flex items-center justify-between p-2 bg-colorBlue w-full h-[60px] text-white font-semibold"
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  <span className="text-lg">{selectedOption}</span>
+                  <Image
+                    src="/icon/dropdown.svg"
+                    alt="DropDown"
+                    height={15}
+                    width={15}
+                  />
+                </button>
+                {isOpen && (
+                  <div className="absolute top-full left-0 w-full bg-primary border border-primaryGray shadow-lg z-10">
+                    {options.map((option) => (
+                      <button
+                        key={option}
+                        className={`w-full text-left p-2 text-black hover:bg-gray-200 ${
+                          selectedOption === option ? "font-bold" : ""
+                        }`}
+                        onClick={() => {
+                          setSelectedOption(option);
+                          setIsOpen(false);
+                        }}
+                      >
+                        {option}
+                      </button>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      </AnimatePresence>
+
       <div className="bg-colorDirtyWhite w-full max-w-[428px] h-[677px] mt-1 p-2 overflow-y-auto">
         <div className="bg-primary w-full h-[40px] flex items-center">
           <div className="text-tertiary flex justify-between px-2 w-full">
@@ -131,25 +155,35 @@ const OrdersView = () => {
           </p>
         </div>
 
-        {/* SELECTED ORDERED ITEMS */}
-        <div className="divide-y">
+        <div className="divide-y opacity-50 font-semibold">
           {selectedProducts.map((item, idx) => (
             <motion.div
               key={idx}
+              onMouseDown={() => startLongPress(idx)}
+              onTouchStart={() => startLongPress(idx)}
+              onMouseUp={cancelLongPress}
+              onMouseLeave={cancelLongPress}
+              onTouchEnd={cancelLongPress}
               onClick={(e) => {
                 e.stopPropagation();
-                setSelectedIdx(idx);
+                if (!isEditingQty) {
+                  setSelectedIndexes((prev) =>
+                    prev.includes(idx)
+                      ? prev.filter((i) => i !== idx)
+                      : [...prev, idx]
+                  );
+                }
               }}
               className={`flex justify-between items-center py-2 px-2 text-primary cursor-pointer ${
-                selectedIdx === idx ? "bg-[#ceb395]" : ""
+                selectedIndexes.includes(idx) ? "bg-[#ceb395]" : ""
               }`}
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
             >
               <div className="w-1/2">
                 <div className="flex items-baseline gap-2">
-                  <p className="font-medium">{item.imageTitle}</p>
-                  <p className="text-sm text-gray-500">
+                  <p className="font-semibold">{item.imageTitle}</p>
+                  <p className="text-sm text-primary">
                     <strong className="text-[10px]">
                       {getSizeName(item.size)}
                     </strong>
@@ -157,13 +191,28 @@ const OrdersView = () => {
                 </div>
               </div>
               <p className="w-1/4 text-center">
-                {isEditingQty && selectedIdx === idx ? (
+                {isEditingQty &&
+                selectedIndexes.length === 1 &&
+                selectedIndexes[0] === idx ? (
                   <div onClick={(e) => e.stopPropagation()}>
                     <input
                       type="number"
                       min={1}
                       value={tempQty ?? 1}
                       onChange={(e) => setTempQty(Number(e.target.value))}
+                      onBlur={() => {
+                        if (tempQty !== null) {
+                          useOrderStore.setState((state) => {
+                            const update = [...state.selectedProducts];
+                            update[idx].quantity = tempQty;
+                            return { selectedProducts: update };
+                          });
+                        }
+                        setIsEditingQty(false);
+                        setSelectedIndexes([]);
+                        setTempQty(null);
+                      }}
+                      autoFocus
                       className="w-12 text-center border rounded"
                     />
                   </div>
@@ -171,6 +220,7 @@ const OrdersView = () => {
                   item.quantity
                 )}
               </p>
+
               <p className="w-1/4 text-right">
                 {(item.price[item.size] * (item.quantity || 1)).toFixed(2)}
               </p>
@@ -179,21 +229,27 @@ const OrdersView = () => {
         </div>
       </div>
 
-      {/* TOTAL & PAY */}
       <div className="flex gap-[4px] w-full mt-1">
         <div className="flex-[3] h-[60px] bg-primary menu-total flex items-center text-[24px] px-2 text-white">
           <div className="flex justify-between w-full">
             <p>Total</p>
-            <p>{`₱ ${total.toFixed(2)}`}</p>
+            <p>{`\u20B1 ${total.toLocaleString("en-PH", {
+              minimumFractionDigits: 2,
+            })}`}</p>
           </div>
         </div>
-
         <Button
           variant="pay"
           className="flex-[1]"
           disabled={selectedProducts.length === 0}
+          onClick={() => {
+            if (isPaying) {
+              onClearOrders();
+            }
+            setIsPaying((prev) => !prev);
+          }}
         >
-          PAY
+          {isPaying ? "ORDERS" : "PAY"}
         </Button>
       </div>
     </SectionContainer>
