250a7152ee84c481b69715e93ec2394dfa649cb2
Josh <joshuacolobong11@gmail.com>
2025-05-04 21:40:09 +0800

    Updated Backend

--- backend/script.ts
+++ backend/script.ts
@@ -1,11 +1,41 @@
 import {PrismaClient} from '@prisma/client'
-import { create } from 'domain'
+
 const prisma = new PrismaClient()
 
 async function main(){
-    // to create
-    //const user = await prisma.user.create({data: {
-    // }})
+   
+   // const createProductData = await prisma.category.create({data: {
+   //    name: "WEAK",
+   //    products: {
+   //       create: {
+   //          name: "Nescafe",
+   //          variants: {
+   //             create: [
+   //                {size: "Small", price: 39},
+   //                {size: "Medium", price: 49},
+   //                {size: "Large", price: 59}
+   //             ]
+   //          }
+   //       }
+   //    }
+      
+   // },include: {
+   //    products: {
+   //       include:{
+   //          variants: {
+   //             select: {
+   //                size: true,
+   //                price: true
+   //             }
+   //          }
+   //       }
+   //    }
+   // }})
+   
+   
+
+   // console.log(createProductData)
+
 }
 
 main()
